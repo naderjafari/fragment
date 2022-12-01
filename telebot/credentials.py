dc892c82d4711d4cde0b7c98606ae202b009ec78
Nader Jafari <n.jafari@sain.ir>
2022-12-01 17:26:47 +0330

    Update credentials.py

--- telebot/credentials.py
+++ telebot/credentials.py
@@ -1,3 +1,3 @@
 bot_token = "5954723117:AAHZM96qtZxjjL-jWqRuJ3_PX--Uw4aTb2g"
 bot_user_name = "FragmentAlertBot"
-URL = "the heroku app link that we will create later"
+URL = "https://fragment-naderjafari.vercel.app/"
