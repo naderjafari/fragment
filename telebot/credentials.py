66d5780a1cf04fc01b1826e958f0bc86752ef69c
Nader Jafari <n.jafari@sain.ir>
2022-12-02 17:59:49 +0330

    Debug

--- telebot/credentials.py
+++ telebot/credentials.py
@@ -2,5 +2,10 @@ import os
 
 bot_token = os.environ.get('API_KEY')
 print('Bot Token:',bot_token)
-bot_user_name = "FragmentAlertBot"
-URL = "https://fragment-sandy.vercel.app/"
+bot_user_name = os.environ.get('USER_NAME')
+print('Bot User Name:',bot_user_name)
+
+URL = os.environ.get('URL')
+
+print('Bot User Name:',bot_user_name)
+
